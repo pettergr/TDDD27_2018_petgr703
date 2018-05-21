9006e18554d683de6dbbd5d0eb7543fa04a22b18
Petter Granli <petter@temart.com>
2018-05-22 01:55:47 +0200

    Cleanup of unused old code

--- client/src/actions/customerActions.js
+++ client/src/actions/customerActions.js
@@ -1,76 +1,6 @@
 import store from '../store/store';
 import axios from "axios";
 
-/*const actions = store => ({
-    getCustomers() {
-        store.setState({customersLoading: true});
-
-        axios
-            .get("/customer")
-            .then(response => {
-                store.setState({
-                    customers: response.data,
-                    customersLoading: false
-                });
-            })
-            .catch(error => {
-                store.setState({
-                    error: true,
-                    customersLoading: false
-                });
-            });
-    },
-
-    deleteCustomer(state, customer) {
-        store.setState({customersLoading: true});
-        const customerId = customer._id;
-        axios
-            .delete(`/customer/${customerId}`)
-            .then(response => {
-                store.setState({
-                    customers: state.customers.filter(element => element._id !== customerId),
-                    customersLoading: false
-                });
-            })
-            .catch(error => {
-                store.setState({
-                    error: true,
-                    customersLoading: false
-                });
-            });
-    },
-
-    addCustomer(state, customerName) {
-        store.setState({customersLoading: true});
-        axios
-            .post("/customer", { name: customerName })
-            .then(response => {
-                store.setState({
-                    customers: [...state.customers, response.data],
-                    customersLoading: false
-                });
-            })
-            .catch(error => {
-                store.setState({
-                    error: true,
-                    customersLoading: false
-                });
-            });
-    }
-});*/
-
-/*export function getCustomers() {
-    console.log("inne i getCustomers")
-    return {
-        type: 'SET_CUSTOMERS',
-        payload: {
-            request:{
-            url:'/customer'
-            }
-        }
-    }
-}*/
-
 export const getCustomers = (): Action => ({
     type: 'SET_CUSTOMERS',
     payload: {
@@ -102,23 +32,3 @@ export const deleteCustomer = (customerId): Action => ({
         }
     }
 });
-
-/*
-deleteCustomer(state, customer) {
-    store.setState({customersLoading: true});
-    const customerId = customer._id;
-    axios
-        .delete(`/customer/${customerId}`)
-        .then(response => {
-            store.setState({
-                customers: state.customers.filter(element => element._id !== customerId),
-                customersLoading: false
-            });
-        })
-        .catch(error => {
-            store.setState({
-                error: true,
-                customersLoading: false
-            });
-        });
-},*/
