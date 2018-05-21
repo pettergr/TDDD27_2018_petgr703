2aa8877726ceb5a436944c4d1bf2414e7d2127ce
Petter Granli <petter@temart.com>
2018-05-22 01:44:29 +0200

    Removed old code

--- client/src/reducers/customerReducer.js
+++ client/src/reducers/customerReducer.js
@@ -1,15 +1,12 @@
 const customerReducer = (state = {customers: []}, action) => {
     switch (action.type) {
         case 'SET_CUSTOMERS_SUCCESS':
-            console.log("SET_CUSTOMERS")
-            console.log(action.payload.data)
             return Object.assign({}, state, {
                 customers: [
                     ...action.payload.data
                 ]
             })
         case 'ADD_CUSTOMER_SUCCESS':
-            console.log(action);
             return Object.assign({}, state, {
                 customers: [
                     ...state.customers,
@@ -18,7 +15,6 @@ const customerReducer = (state = {customers: []}, action) => {
             })
 
         case 'DELETE_CUSTOMER_SUCCESS':
-            console.log(action.meta.previousAction.payload.request.url.substring(10));
             let custId = action.meta.previousAction.payload.request.url.substring(10);
             return Object.assign({}, state, {
                 customers: state.customers.filter(elem => elem._id !== custId),
